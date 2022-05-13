62eae16edf82b309cb98a9e022441815e4c95d15
binitha-experionglobal <binitha.s@experionglobal.com>
2022-05-13 13:23:57 +0530

    adds changes to index file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import "./styles.css";
+import Categories from "./pages/Categories/Categories";
+import Customers from "./pages/Customers/Customers";
+import Orders from "./pages/Orders/Orders";
+import Users from "./pages/Users/Users";
+import MyProfile from "./pages/My-Profile/MyProfile";
+import Notfound from "./pages/NotFound/Notfound";
+import Pro from "./pages/My-Profile/pro"
+import ProtectedRoute from "./components/ProtectedRoute"
+import UnAuthorized from "./pages/UnAuthorized/unAuthorized";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<ProtectedRoute />}>
+          <Route exact path="/home" element={<Home />} />
+        </Route>
+        <Route path="/home/categories" element={<ProtectedRoute />}>
+          <Route exact path="/home/categories" element={<Categories />} />
+        </Route>
+        <Route path="/home/customers" element={<ProtectedRoute />}>
+          <Route exact path="/home/customers" element={<Customers />} />
+        </Route>
+        <Route path="/home/orders" element={<ProtectedRoute />}>
+          <Route exact path="/home/orders" element={<Orders />} />
+        </Route>
+        <Route path="/home/users" element={<ProtectedRoute />}>
+          <Route exact path="/home/users" element={<Users />} />
+        </Route>
+
+        <Route path="/unauthorized-access" element={<UnAuthorized />} />
+        
+        <Route path="/home/my-profile" element={<ProtectedRoute />}>
+          <Route exact path="/home/my-profile" element={<MyProfile/>} />
+        </Route>
+        <Route path="home/pro" element={<Pro/>}/>
+        
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,59 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router";
 import "./styles.css";
-import Categories from "./pages/Categories/Categories";
-import Customers from "./pages/Customers/Customers";
-import Orders from "./pages/Orders/Orders";
-import Users from "./pages/Users/Users";
-import MyProfile from "./pages/My-Profile/MyProfile";
-import Notfound from "./pages/NotFound/Notfound";
-import Pro from "./pages/My-Profile/pro"
-import ProtectedRoute from "./components/ProtectedRoute"
-import UnAuthorized from "./pages/UnAuthorized/unAuthorized";
-
-export default function RouteApp() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<ProtectedRoute />}>
-          <Route exact path="/home" element={<Home />} />
-        </Route>
-        <Route path="/home/categories" element={<ProtectedRoute />}>
-          <Route exact path="/home/categories" element={<Categories />} />
-        </Route>
-        <Route path="/home/customers" element={<ProtectedRoute />}>
-          <Route exact path="/home/customers" element={<Customers />} />
-        </Route>
-        <Route path="/home/orders" element={<ProtectedRoute />}>
-          <Route exact path="/home/orders" element={<Orders />} />
-        </Route>
-        <Route path="/home/users" element={<ProtectedRoute />}>
-          <Route exact path="/home/users" element={<Users />} />
-        </Route>
-
-        <Route path="/unauthorized-access" element={<UnAuthorized />} />
-        
-        <Route path="/home/my-profile" element={<ProtectedRoute />}>
-          <Route exact path="/home/my-profile" element={<MyProfile/>} />
-        </Route>
-        <Route path="home/pro" element={<Pro/>}/>
-        
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouteApp />
+    <App />
   </React.StrictMode>
 );
 
